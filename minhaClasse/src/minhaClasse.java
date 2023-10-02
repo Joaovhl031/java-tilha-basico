091121d81a895f5726b1dab950c121a0890b81c8
Joao <joaohenrique9021@gmail.com>
2023-10-02 16:43:39 -0300

    inputs

--- minhaClasse/src/minhaClasse.java
+++ minhaClasse/src/minhaClasse.java
@@ -1,9 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class minhaClasse {
     public static void main(String[] args) {
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        Scanner print = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("digite seu nome: ");
+        String nome = print.next();
+
+        System.out.println("digite seu sobrenome: ");
+        String sobrenome = print.next();
+
+        System.out.println("sua idade: ");
+        int idade = print.nextInt();
+
+        System.out.println("digite sua altura: ");
+        double altura = print.nextDouble();
 
         System.out.println("Ola, meu nome é "+nome+" "+sobrenome);
         System.out.println("Tenho "+idade+" anos");
