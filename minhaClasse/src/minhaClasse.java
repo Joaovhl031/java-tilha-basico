abd44026f22fba3705fe91b739eb07c99eb598e5
Joao <joaohenrique9021@gmail.com>
2023-10-02 16:44:35 -0300

    corrigindo scanner

--- minhaClasse/src/minhaClasse.java
+++ minhaClasse/src/minhaClasse.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 
 public class minhaClasse {
     public static void main(String[] args) {
-        Scanner print = new Scanner(System.in).useLocale(Locale.US);
+        try (Scanner print = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("digite seu nome: ");
+            String nome = print.next();
 
-        System.out.println("digite seu nome: ");
-        String nome = print.next();
+            System.out.println("digite seu sobrenome: ");
+            String sobrenome = print.next();
 
-        System.out.println("digite seu sobrenome: ");
-        String sobrenome = print.next();
+            System.out.println("sua idade: ");
+            int idade = print.nextInt();
 
-        System.out.println("sua idade: ");
-        int idade = print.nextInt();
+            System.out.println("digite sua altura: ");
+            double altura = print.nextDouble();
 
-        System.out.println("digite sua altura: ");
-        double altura = print.nextDouble();
-
-        System.out.println("Ola, meu nome é "+nome+" "+sobrenome);
-        System.out.println("Tenho "+idade+" anos");
-        System.out.println("minha altura é "+altura);
+            System.out.println("Ola, meu nome é "+nome+" "+sobrenome);
+            System.out.println("Tenho "+idade+" anos");
+            System.out.println("minha altura é "+altura);
+        }
 
     }
 }
